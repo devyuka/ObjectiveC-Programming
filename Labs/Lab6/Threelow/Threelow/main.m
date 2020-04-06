2cd4b5d100a8f1574d2dc6cc7d37b5bc614ae84b
Yuka <54751741+Yuka030@users.noreply.github.com>
2020-04-06 16:01:53 -0700

    Implement dice rolling from user input

--- Labs/Lab6/Threelow/Threelow/main.m
+++ Labs/Lab6/Threelow/Threelow/main.m
@@ -9,9 +9,29 @@ int main(int argc, const char * argv[]) {
         Dice *dice4 = [Dice new];
         Dice *dice5 = [Dice new];
         NSArray *diceArray = @[dice1, dice2, dice3, dice4, dice5];
-        for(Dice *dice in diceArray){
-            NSLog(@"%ld", (long)dice.currentValue);
-        }
+        
+        while (TRUE) {
+            
+            printf("'roll' to roll the dice\n");
+            
+            char str[10];
+            fgets (str, 10, stdin);
+            
+            NSString *inputString = [[NSString alloc] initWithUTF8String:str];
+            
+            inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            
+                if([inputString isEqualToString:@"roll"]){
+                    for(Dice *dice in diceArray){
+                        [dice randomizeValue];
+                    }
+                }
+                for(Dice *dice in diceArray){
+                    NSLog(@"%ld", (long)dice.currentValue);
+                }
+                
+            }
+        
     }
     return 0;
 }
